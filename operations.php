1f5f3dfbbb172d00d5b2b6d14f19fb37f16cdffb
bhanu <bhanudamir@gmail.com>
2015-06-06 18:09:36 +0530

    Operations.php is committed without being staged

---
+++ operations.php
@@ -0,0 +1,29 @@
+<?php
+include 'db/db.php';
+class operations{
+	private $categoryId;
+
+	public function  __construct($a){
+		$this->categoryId = $a;
+		
+	}
+	public function create($dbArr){
+		$dbaseObj = new dbase($this->categoryId,'create');
+		$dbaseArr = $dbaseObj->action($dbArr);
+	}
+	public function deletes($row){
+		$dbaseObj = new dbase($this->categoryId,'delete');
+		$dbaseArr = $dbaseObj->action($row);
+        }
+	public function view(){
+		$dbaseObj = new dbase($this->categoryId,'view');
+		$dbaseArr = $dbaseObj->action(0);
+		return ($dbaseArr);
+        }
+
+	public function edit($dbArr){
+		$dbaseObj = new dbase($this->categoryId,'edit');
+		$dbaseArr = $dbaseObj->action($dbArr);
+        }
+}
+?>
